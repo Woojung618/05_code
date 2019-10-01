f92d2730ad9048ec21c0c4c6b4b6ad64f0ae74da
Woojung618 <eyes_cat@naver.com>
2019-10-01 15:57:18 +0900

    05_4

--- main.c
+++ main.c
@@ -4,8 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=0; 
-	for (i;i<10;i++)
-		printf("Hello World!\n");
-	
+	int i=0; int meter=0;
+	while (i<3){
+		meter = i*1609;
+		printf("%d마일은 %d미터입니다.\n",i,meter);
+		i++;
+	}
 }
